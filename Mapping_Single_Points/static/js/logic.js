527ac573c4f25989f7c389f75f1fc008596f25bf
AndrewZinc <107150022+AndrewZinc@users.noreply.github.com>
2022-10-18 17:52:20 -0400

    Added code for skill drill.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,10 +5,22 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    zoomControl: true,
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+var marker = L.marker([42.142872, -71.516830]).addTo(map);
+
+L.circle([34.0522, -118.2437], {
+    stroke: 'true',
+    color: 'black',
+    fill: 'true',
+    fillColor: '#ffff66',
+    fillOpacity: 0.6,
+    radius: 300
+ }).addTo(map);

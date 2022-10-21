fe7923b33cb7e500cd0c9bf3b168aba87e0fc615
AndrewZinc <107150022+AndrewZinc@users.noreply.github.com>
2022-10-21 16:51:17 -0400

    Updated map logic so that street layer is displayed by default.

--- Mapping_GeoJSON_Points/static/js/logic1.js
+++ Mapping_GeoJSON_Points/static/js/logic1.js
@@ -23,31 +23,60 @@ let baseMaps = {
 
 // Create a map object centered on the coords, with zoom level 4.
 let map = L.map("mapid", {
-   center: [30, 3],
+   center: [30, 30],
    zoom: 2,
-   layer: [streets]
+   layers: [streets]
 });
 
 // Pass our map layers into the layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
+var popup = L.popup()
+   .setLatLng([51.513, -0.09])
+   .setContent("I am a standalone popup.")
+   .addTo(map);
+
+
+var popup = L.popup();
+
+// function onMapClick(e) {
+//    popup
+//       .setLatLng(e.latlng)
+//       .setContent("You clicked the map at " + e.latlng.toString())
+//       .openOn(map);
+// };
+
+// map.on('click', onMapClick);
+
 let airData = d3.json("https://raw.githubusercontent.com/AndrewZinc/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/majorAirports.json");
+let bData = "https://raw.githubusercontent.com/AndrewZinc/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/majorAirports.json";
+
+console.log(airData);
 
 airData.then(function(data) {
    // Place our GeoJSON feature on the map.
    L.geoJSON(data).addTo(map);
   });
 
-  console.log("starting L.geoJSON(airData");
+
   
-L.geoJSON(airData, {
+var marker = L.marker([42.142872, -71.516830]).addTo(map)
+   .bindPopup('A town in Massachusetts.');
+
+// console.log("starting marker layer popups");
+
+// L.marker(airData, {
+//    onEachFeature: function(feature, layer) {
+//       console.log("layer ");
+//       layer.bindPopup("<h2>Airport Code: " + feature.properties.faa +"<hr>Airport Name: "+ feature.properties.name +"</h2>");
+//    }
+// }).addTo(map);
+
+console.log("starting geoJSON layer popups");
+
+var layerGroup =  L.geoJSON(bData, {
    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup().bindPopup("<h2>Airport Code: " + feature.properties.faa +"<hr>Airport Name: "+ feature.properties.name +"</h2>");
-      ;
+      layer.bindPopup("<h2>Airport Code: " + feature.properties.faa +"<hr>Airport Name: "+ feature.properties.name +"</h2>");
    }
-});
-
-
-
+}).addTo(map);
 

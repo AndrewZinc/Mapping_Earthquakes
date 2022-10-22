4f890c0dde1734f300e3c1d3f266a0589d416727
AndrewZinc <107150022+AndrewZinc@users.noreply.github.com>
2022-10-22 12:54:21 -0400

    Finished plotting polygons for Toronto neighborhoods.  Added custom attributes and popups.

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,28 +1,32 @@
 
-let flightData = "torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/AndrewZinc/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
-console.log(flightData);
+console.log(torontoHoods);
 
-d3.json(flightData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
    console.log(data);
    // Place our GeoJSON feature on the map.
    createFeatures(data.features);
   });
 
  
-function createFeatures(flightData) {
+function createFeatures(torontoHoods) {
    function onEachFeature (feature, layer) {
-      layer.bindPopup("<h2>Airline: " + feature.properties.airline +"<hr>Destination: "+ feature.properties.dst +"</h2>");
+      layer.bindPopup("<h2>Neighborhood: " + feature.properties.AREA_NAME +"</h2>");
    }
 
-   var flights = L.geoJSON(flightData, {
-      onEachFeature: onEachFeature
+   var neighborhoods = L.geoJSON(torontoHoods, {
+      onEachFeature: onEachFeature,
+      color: "blue",
+      weight: 1,
+      fillColor: "yellow",
+      fillOpacity: 0.4
    });
 
-   createMap(flights);
+   createMap(neighborhoods);
 };
   
-function createMap(flights) {
+function createMap(neighborhoods) {
 // We create the tile layer that will be the background of our map.
    var streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
@@ -32,7 +36,7 @@ function createMap(flights) {
    });
 
    // We create the tile layer that will be the background of our map.
-   var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+   var satelliteStreets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
       maxZoom: 18,
       zoomControl: true,
@@ -42,18 +46,18 @@ function createMap(flights) {
    // Create a base layer that holds both maps.
    var baseMaps = {
       "Street Map": streets,
-      "Dark Map": dark
+      "Satellite Streets": satelliteStreets
    };
 
    var overlayMaps = {
-      "Toronto Flights": flights
+      "Toronto Neighborhoods": neighborhoods
    };
 
-   // Create a map object centered on the coords, with zoom level 2.
+   // Create a map object centered on the coords, with zoom level 11.
    var map = L.map("mapid", {
-      center: [44, -80.0],
-      zoom: 2,
-      layers: [streets, flights]
+      center: [43.7, -79.3],
+      zoom: 11,
+      layers: [satelliteStreets]
    });
 
 

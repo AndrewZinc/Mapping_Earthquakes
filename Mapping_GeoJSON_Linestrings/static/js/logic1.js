6edb61e0931f0b898429be1c6dd5d63f87b64310
AndrewZinc <107150022+AndrewZinc@users.noreply.github.com>
2022-10-22 11:20:53 -0400

    Finally got the code structure and popups working.

--- Mapping_GeoJSON_Linestrings/static/js/logic1.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic1.js
@@ -1,82 +1,67 @@
 
-// We create the tile layer that will be the background of our map.
-let light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-    maxZoom: 18,
-    zoomControl: true,
-    accessToken: API_KEY
-});
-
-// We create the tile layer that will be the background of our map.
-let dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-    maxZoom: 18,
-    zoomControl: true,
-    accessToken: API_KEY
-});
-
-// Create a base layer that holds both maps.
-let baseMaps = {
-   Light: light,
-   Dark: dark
-};
-
-// Create a map object centered on the coords, with zoom level 4.
-let map = L.map("mapid", {
-   center: [44, -80.0],
-   zoom: 2,
-   layers: [light]
-});
-
-// Pass our map layers into the layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
-
-// var popup = L.popup()
-//    .setLatLng([51.513, -0.09])
-//    .setContent("I am a standalone popup.")
-//    .addTo(map);
-
-
-// var popup = L.popup();
-
-// function onMapClick(e) {
-//    popup
-//       .setLatLng(e.latlng)
-//       .setContent("You clicked the map at " + e.latlng.toString())
-//       .openOn(map);
-// };
-
-// map.on('click', onMapClick);
 let flightData = "torontoRoutes.json";
-let airData = d3.json("https://raw.githubusercontent.com/AndrewZinc/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/majorAirports.json");
-let bData = "https://raw.githubusercontent.com/AndrewZinc/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/majorAirports.json";
 
 console.log(flightData);
 
 d3.json(flightData).then(function(data) {
+   console.log(data);
    // Place our GeoJSON feature on the map.
-   L.geoJSON(data).addTo(map);
+   createFeatures(data.features);
   });
 
+ 
+function createFeatures(flightData) {
+   function onEachFeature (feature, layer) {
+      layer.bindPopup("<h2>Airline: " + feature.properties.airline +"<hr>Destination: "+ feature.properties.dst +"</h2>");
+   }
 
+   var flights = L.geoJSON(flightData, {
+      onEachFeature: onEachFeature
+   });
+
+   createMap(flights);
+};
   
-// var marker = L.marker([42.142872, -71.516830]).addTo(map)
-//    .bindPopup('A town in Massachusetts.');
+function createMap(flights) {
+// We create the tile layer that will be the background of our map.
+   var streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+      maxZoom: 18,
+      zoomControl: true,
+      accessToken: API_KEY
+   });
 
-// console.log("starting marker layer popups");
+   // We create the tile layer that will be the background of our map.
+   var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+      maxZoom: 18,
+      zoomControl: true,
+      accessToken: API_KEY
+   });
 
-// L.marker(airData, {
-//    onEachFeature: function(feature, layer) {
-//       console.log("layer ");
-//       layer.bindPopup("<h2>Airport Code: " + feature.properties.faa +"<hr>Airport Name: "+ feature.properties.name +"</h2>");
-//    }
-// }).addTo(map);
+   // Create a base layer that holds both maps.
+   var baseMaps = {
+      "Street Map": streets,
+      "Dark Map": dark
+   };
 
-// console.log("starting geoJSON layer popups");
+   var overlayMaps = {
+      "Toronto Flights": flights
+   };
 
-// var layerGroup =  L.geoJSON(bData, {
-//    onEachFeature: function(feature, layer) {
-//       layer.bindPopup("<h2>Airport Code: " + feature.properties.faa +"<hr>Airport Name: "+ feature.properties.name +"</h2>");
-//    }
-// }).addTo(map);
+   // Create a map object centered on the coords, with zoom level 2.
+   var map = L.map("mapid", {
+      center: [44, -80.0],
+      zoom: 2,
+      layers: [streets, flights]
+   });
+
+
+
+   // Pass our map layers into the layers control and add the layers control to the map.
+   L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+   }).addTo(map);
+};
+
 
--- Mapping_GeoJSON_Points/static/js/logic1.js
+++ Mapping_GeoJSON_Points/static/js/logic1.js
@@ -1,82 +1,63 @@
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-    maxZoom: 18,
-    zoomControl: true,
-    accessToken: API_KEY
-});
-
-// We create the tile layer that will be the background of our map.
-let dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-    maxZoom: 18,
-    zoomControl: true,
-    accessToken: API_KEY
-});
-
-// Create a base layer that holds both maps.
-let baseMaps = {
-   Street: streets,
-   Dark: dark
-};
-
-// Create a map object centered on the coords, with zoom level 4.
-let map = L.map("mapid", {
-   center: [30, 30],
-   zoom: 2,
-   layers: [streets]
-});
-
-// Pass our map layers into the layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
-
-var popup = L.popup()
-   .setLatLng([51.513, -0.09])
-   .setContent("I am a standalone popup.")
-   .addTo(map);
-
-
-var popup = L.popup();
-
-// function onMapClick(e) {
-//    popup
-//       .setLatLng(e.latlng)
-//       .setContent("You clicked the map at " + e.latlng.toString())
-//       .openOn(map);
-// };
-
-// map.on('click', onMapClick);
-
 let airData = d3.json("https://raw.githubusercontent.com/AndrewZinc/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/majorAirports.json");
-let bData = "https://raw.githubusercontent.com/AndrewZinc/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/majorAirports.json";
 
 console.log(airData);
 
 airData.then(function(data) {
    // Place our GeoJSON feature on the map.
-   L.geoJSON(data).addTo(map);
+   createFeatures(data.features);
   });
 
-
-  
-var marker = L.marker([42.142872, -71.516830]).addTo(map)
-   .bindPopup('A town in Massachusetts.');
-
-// console.log("starting marker layer popups");
-
-// L.marker(airData, {
-//    onEachFeature: function(feature, layer) {
-//       console.log("layer ");
-//       layer.bindPopup("<h2>Airport Code: " + feature.properties.faa +"<hr>Airport Name: "+ feature.properties.name +"</h2>");
-//    }
-// }).addTo(map);
-
-console.log("starting geoJSON layer popups");
-
-var layerGroup =  L.geoJSON(bData, {
-   onEachFeature: function(feature, layer) {
+function createFeatures(flightData) {
+   function onEachFeature (feature, layer) {
       layer.bindPopup("<h2>Airport Code: " + feature.properties.faa +"<hr>Airport Name: "+ feature.properties.name +"</h2>");
    }
-}).addTo(map);
+
+   var flights = L.geoJSON(flightData, {
+      onEachFeature: onEachFeature
+   });
+
+   createMap(flights);
+};
+  
+function createMap(flights) {
+// We create the tile layer that will be the background of our map.
+   var streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+      maxZoom: 18,
+      zoomControl: true,
+      accessToken: API_KEY
+   });
+
+   // We create the tile layer that will be the background of our map.
+   var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+      maxZoom: 18,
+      zoomControl: true,
+      accessToken: API_KEY
+   });
+
+   // Create a base layer that holds both maps.
+   var baseMaps = {
+      "Street Map": streets,
+      "Dark Map": dark
+   };
+
+   var overlayMaps = {
+      "Major Airports": flights
+   };
+
+   // Create a map object centered on the coords, with zoom level 4.
+   var map = L.map("mapid", {
+      center: [30, 30],
+      zoom: 2,
+      layers: [streets, flights]
+   });
+
+   // Pass our map layers into the layers control and add the layers control to the map.
+   L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+   }).addTo(map);
+};
+
 
